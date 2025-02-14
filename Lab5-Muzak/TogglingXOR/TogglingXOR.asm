9eeb54ed3493505b28d21d15e714ec0afc311a25
andrew1593571 <andrewkeller2002@gmail.com>
2025-02-14 09:30:17 -0700

    Init Lab 5 Toggline XOR

---
+++ Lab5-Muzak/TogglingXOR/TogglingXOR.asm
@@ -0,0 +1,12 @@
+;******************************************************************************
+; *
+; Filename: FileName.asm *
+; Date: February 14, 2025 *
+; Author: Andrew Keller *
+; Company: Idaho State University *
+; Description: Sends toggling data out Port C b0, reads from Port B B0 *
+; Github: https://github.com/andrew1593571/RCET3375.git *
+; *
+;******************************************************************************
+  
+
