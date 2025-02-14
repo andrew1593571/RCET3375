9ce34e76044930548380bc8333ea8621ff8282cb
andrew1593571 <andrewkeller2002@gmail.com>
2025-02-14 10:21:57 -0700

    Init Lab 5 1kHzSquareWave

---
+++ Lab5-Muzak/1kHzSquareWave/1kHzSquareWave.asm
@@ -0,0 +1,34 @@
+;******************************************************************************
+; *
+; Filename: FileName.asm *
+; Date: February 14, 2025 *
+; Author: Andrew Keller *
+; Company: Idaho State University *
+; Description: Sends a  *
+; Github: https://github.com/andrew1593571/RCET3375.git *
+; *
+;******************************************************************************
+  
+#include "p16f883.inc"
+
+; CONFIG1
+; __config 0xE0F1
+ __CONFIG _CONFIG1, _FOSC_XT & _WDTE_OFF & _PWRTE_OFF & _MCLRE_ON & _CP_OFF & _CPD_OFF & _BOREN_OFF & _IESO_OFF & _FCMEN_OFF & _LVP_OFF
+; CONFIG2
+; __config 0xFFFF
+ __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
+  
+    ORG H'000'
+    GOTO SETUP
+    
+    ORG H'004'
+    RETFIE
+    
+SETUP:
+
+    GOTO MAIN
+    
+MAIN:
+
+    GOTO MAIN	    ;LOOP INDEFINITELY
+    END
