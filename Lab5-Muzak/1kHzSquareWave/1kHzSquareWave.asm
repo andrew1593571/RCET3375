fef0d2900464aa370b3c7c93795a6ac6fb9c0d40
andrew1593571 <andrewkeller2002@gmail.com>
2025-02-14 11:37:53 -0700

    Squarewave Subroutine, Main

--- Lab5-Muzak/1kHzSquareWave/1kHzSquareWave.asm
+++ Lab5-Muzak/1kHzSquareWave/1kHzSquareWave.asm
@@ -1,10 +1,10 @@
 ;******************************************************************************
 ; *
-; Filename: FileName.asm *
+; Filename: 1kHzSquareWave.asm *
 ; Date: February 14, 2025 *
 ; Author: Andrew Keller *
 ; Company: Idaho State University *
-; Description: Sends a  *
+; Description: Sends a 1kHz square wave out RC0 *
 ; Github: https://github.com/andrew1593571/RCET3375.git *
 ; *
 ;******************************************************************************
@@ -24,11 +24,57 @@
     ORG H'004'
     RETFIE
     
+CBLOCK H'20'	    ;DEFINES A LIST OF CONSTANTS AT SPECIFIC ADDRESSES
+    COUNT	    ;ADDRESS 0x20
+ENDC
+    
 SETUP:
+    BANKSEL ANSELH  ;DISABLE ANALOG INPUTS ON PORT B
+    MOVLW H'00'
+    MOVWF ANSELH
+    
+    BANKSEL IOCB    ;DISABLE INTERRUPT ON CHANGE FOR PORT B
+    MOVLW H'00'
+    MOVWF IOCB
+    
+    BANKSEL PORTB   ;CLEAR PORT B, SET TRISTATE B TO INPUT
+    CLRF PORTB	    
+    BANKSEL TRISB
+    MOVLW H'FF'
+    MOVWF TRISB
+    
+    BANKSEL WPUB    ;DISABLE PORT B PULL-UPS
+    MOVLW H'00'
+    MOVWF WPUB
+    
+    BANKSEL ANSEL   ;DISABLE PORT A ANALOG INPUTS
+    MOVLW H'00'
+    MOVWF ANSEL
+    BANKSEL PORTA   ;CLEAR PORT A, SET AS OUTPUTS
+    CLRF PORTA
+    BANKSEL TRISA
+    MOVLW H'00'
+    MOVWF TRISA
 
+    BANKSEL PORTA
     GOTO MAIN
     
+SQUAREWAVE:
+    MOVLW H'01'
+    XORWF PORTA, 1
+    MOVLW H'A3'
+    MOVWF COUNT
+    
+LOOP:
+    DECFSZ COUNT
+    GOTO LOOP
+    
+    NOP
+    RETURN
+    
 MAIN:
-
+    BTFSC PORTB, 0  ;IF RB0 IS HIGH, CALL SQUAREWAVE
+    CALL SQUAREWAVE
+    
     GOTO MAIN	    ;LOOP INDEFINITELY
     END
