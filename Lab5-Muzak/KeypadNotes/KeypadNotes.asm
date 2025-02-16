1f0a5918db2e637aa8ff80f9da14d91ad3de3106
andrew1593571 <andrewkeller2002@gmail.com>
2025-02-15 20:33:58 -0700

    Lab 5 KeypadNotes Init

---
+++ Lab5-Muzak/KeypadNotes/KeypadNotes.asm
@@ -0,0 +1,12 @@
+;******************************************************************************
+; *
+; Filename: KeypadNotes.asm *
+; Date: February 14, 2025 *
+; Author: Andrew Keller *
+; Company: Idaho State University *
+; Description: Plays a different note with each button on a keypad *
+; Github: https://github.com/andrew1593571/RCET3375.git *
+; *
+;******************************************************************************
+  
+
