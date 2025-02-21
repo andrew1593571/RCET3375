c8d8a4ef575dd35cf313a8b6722d46a286b09426
andrew1593571 <andrewkeller2002@gmail.com>
2025-02-21 09:15:29 -0700

    Init Include File Testing

---
+++ IncludeTesting/IncludeTesting.asm
@@ -0,0 +1,12 @@
+;******************************************************************************
+; *
+; Filename: FileName.asm *
+; Date: February 03, 2025 *
+; Author: Andrew Keller *
+; Company: Idaho State University *
+; Description: INPUT DESCRIPTION HERE *
+; Github: https://github.com/andrew1593571/RCET3375.git *
+; *
+;******************************************************************************
+  
+
