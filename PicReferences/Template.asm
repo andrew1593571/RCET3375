243ef5c5d525683417705952138b43d4648ea783
andrew1593571 <andrewkeller2002@gmail.com>
2025-02-21 10:12:15 -0700

    Edit assembly file template

--- PicReferences/Template.asm
+++ PicReferences/Template.asm
@@ -1,7 +1,7 @@
 ;******************************************************************************
 ; *
 ; Filename: FileName.asm *
-; Date: February 03, 2025 *
+; Date: February 21, 2025 *
 ; Author: Andrew Keller *
 ; Company: Idaho State University *
 ; Description: INPUT DESCRIPTION HERE *
@@ -9,4 +9,24 @@
 ; *
 ;******************************************************************************
   
-
+    INCLUDE PIC16F883TestingSetup.inc
+    
+    ;#####____TODO ADD CONFIGURATION BITS____#####
+    
+    ORG H'000'
+    GOTO SETUP
+    
+    ORG H'004'
+    GOTO INTERRUPT
+    
+SETUP:
+    CALL INITIALIZE	    ;CALLS THE INITIALIZE SUBROUTINE IN INCLUDE FILE
+    GOTO MAIN
+    
+INTERRUPT:
+    RETFIE
+    
+MAIN:
+    
+    GOTO MAIN
+    END
