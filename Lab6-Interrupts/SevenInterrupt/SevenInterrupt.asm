d420624e8fb034c7c4cde0f5597d96f3d8265714
andrew1593571 <andrewkeller2002@gmail.com>
2025-02-24 13:06:05 -0700

    Init Lab Six SevenInterrupt

---
+++ Lab6-Interrupts/SevenInterrupt/SevenInterrupt.asm
@@ -0,0 +1,41 @@
+;******************************************************************************
+; *
+; Filename: SevenInterrupt.asm *
+; Date: February 24, 2025 *
+; Author: Andrew Keller *
+; Company: Idaho State University *
+; Description: Displays a 1, Displays 7 when interrupted *
+; Github: https://github.com/andrew1593571/RCET3375.git *
+; *
+;******************************************************************************
+  
+    INCLUDE "PIC16F883SevenInterrupt.inc"
+    #include "p16f883.inc"
+
+; CONFIG1
+; __config 0xE0F1
+ __CONFIG _CONFIG1, _FOSC_XT & _WDTE_OFF & _PWRTE_OFF & _MCLRE_ON & _CP_OFF & _CPD_OFF & _BOREN_OFF & _IESO_OFF & _FCMEN_OFF & _LVP_OFF
+; CONFIG2
+; __config 0xFFFF
+ __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
+    
+    ORG H'000'
+    GOTO SETUP
+    
+    ORG H'004'
+    GOTO INTERRUPT
+    
+SETUP:
+    CALL INITIALIZE	    ;CALLS THE INITIALIZE SUBROUTINE IN INCLUDE FILE
+    BANKSEL PORTC
+    GOTO MAIN
+    
+INTERRUPT:
+    RETFIE
+    
+MAIN:
+    MOVLW H'31'
+    MOVWF PORTC
+    
+    GOTO MAIN
+    END
