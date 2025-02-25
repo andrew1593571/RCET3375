7fa631c2576c1f0de48c52ecf4367b9727dcba33
andrew1593571 <andrewkeller2002@gmail.com>
2025-02-25 14:22:15 -0700

    Configuration Bits

--- Lab6-Interrupts/CombinedInterrupt/CombinedInterrupt.asm
+++ Lab6-Interrupts/CombinedInterrupt/CombinedInterrupt.asm
@@ -10,8 +10,14 @@
 ;******************************************************************************
   
     INCLUDE PIC16F883CombinedInterruptSetup.inc
-    
-    ;#####____TODO ADD CONFIGURATION BITS____#####
+    #include "p16f883.inc"
+
+; CONFIG1
+; __config 0xE0F1
+ __CONFIG _CONFIG1, _FOSC_XT & _WDTE_OFF & _PWRTE_OFF & _MCLRE_ON & _CP_OFF & _CPD_OFF & _BOREN_OFF & _IESO_OFF & _FCMEN_OFF & _LVP_OFF
+; CONFIG2
+; __config 0xFFFF
+ __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
     
     ORG H'000'
     GOTO SETUP
