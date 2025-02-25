8e4100953c2a823fd41dfebfe3909f2e2672a0d7
andrew1593571 <andrewkeller2002@gmail.com>
2025-02-25 13:34:35 -0700

    Init Lab 6 SixInterrupt

---
+++ Lab6-Interrupts/SixInterrupt/SixInterrupt.asm
@@ -0,0 +1,32 @@
+;******************************************************************************
+; *
+; Filename: SixInterrupt.asm *
+; Date: February 25, 2025 *
+; Author: Andrew Keller *
+; Company: Idaho State University *
+; Description: INPUT DESCRIPTION HERE *
+; Github: https://github.com/andrew1593571/RCET3375.git *
+; *
+;******************************************************************************
+  
+    INCLUDE PIC16F883SixInterruptSetup.inc
+    
+    ;#####____TODO ADD CONFIGURATION BITS____#####
+    
+    ORG H'000'
+    GOTO SETUP
+    
+    ORG H'004'
+    GOTO INTERRUPT
+    
+SETUP:
+    CALL INITIALIZE	    ;CALLS THE INITIALIZE SUBROUTINE IN INCLUDE FILE
+    GOTO MAIN
+    
+INTERRUPT:
+    RETFIE
+    
+MAIN:
+    
+    GOTO MAIN
+    END
