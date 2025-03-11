2cd1af40404534eaddadfc067151479c6140248b
andrew1593571 <andrewkeller2002@gmail.com>
2025-03-11 09:42:29 -0600

    Timer ISR

--- Lab8-ADCServo/ADCServo/ADCSetrvo.asm
+++ Lab8-ADCServo/ADCServo/ADCSetrvo.asm
@@ -10,8 +10,15 @@
 ;******************************************************************************
   
     INCLUDE ADCServoSetup.inc
-    
-    ;#####____TODO ADD CONFIGURATION BITS____#####
+    #include "p16f883.inc"
+
+; CONFIG1
+; __config 0xE0F2
+ __CONFIG _CONFIG1, _FOSC_HS & _WDTE_OFF & _PWRTE_OFF & _MCLRE_ON & _CP_OFF & _CPD_OFF & _BOREN_OFF & _IESO_OFF & _FCMEN_OFF & _LVP_OFF
+; CONFIG2
+; __config 0xFFFF
+ __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
+
     
     ORG H'000'
     GOTO SETUP
@@ -24,9 +31,50 @@ SETUP:
     GOTO MAIN
     
 INTERRUPT:
+    BTFSC PIR1, ADIF
+    GOTO ANALOGISR
+    
+    BTFSC PIR1, TMR2IF
+    GOTO TMR2ISR
+    
+    RETFIE
+    
+ANALOGISR:
+    BCF PIR1, ADIF	    ;CLEAR ADIF FLAG
+    MOVF ADRESH, W
+    MOVWF ANALOGSTORE	    ;STORE ANALOG RESULTS IN REGISTER
+    BSF ADCON0, GO
+    RETFIE
+    
+TMR2ISR:
+    BCF PIR1, TMR2IF	    ;CLEAR TMR2IF
+    DECFSZ COUNTLSB	    ;IF COUNTLSB IS NOT 0, RETFIE
+    RETFIE
+    DECFSZ COUNTMSB	    ;IF COUNTMSB IS NOT 0, RETFIE
+    RETFIE
+    
+    BTFSS PORTC, RC0
+    GOTO STARTPW
+    
+    BCF PORTC, RC0
+    MOVLW H'13'
+    MOVWF COUNTMSB
+    MOVLW H'88'
+    MOVWF COUNTLSB
+    RETFIE
+    
+STARTPW:
+    MOVF ADRESH, W
+    ADDLW H'FA'
+    MOVWF COUNTLSB
+    BTFSC STATUS, 0
+    MOVLW H'02'
+    BTFSS STATUS, 0
+    MOVLW H'01'
+    MOVWF COUNTMSB
+    BSF PORTC, RC0
     RETFIE
     
 MAIN:
-    
     GOTO MAIN
     END
