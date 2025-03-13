bf83bb03a84c4927016ba126863f4f69a7dd0bf9
andrew1593571 <andrewkeller2002@gmail.com>
2025-03-13 14:22:59 -0600

    UART setup, manual hex command input validation

--- Lab9-UART/VBSerial/VBSerial/VBSerial/VBSerialForm.Designer.vb
+++ Lab9-UART/VBSerial/VBSerial/VBSerial/VBSerialForm.Designer.vb
@@ -22,17 +22,152 @@ Partial Class VBSerialForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.StatusStrip = New System.Windows.Forms.StatusStrip()
+        Me.SerialComStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.SerialSetupGroupBox = New System.Windows.Forms.GroupBox()
+        Me.ConnectDisconnectButton = New System.Windows.Forms.Button()
+        Me.SerialPortComboBox = New System.Windows.Forms.ComboBox()
+        Me.SerialPort = New System.IO.Ports.SerialPort(Me.components)
+        Me.SerialPortRefreshTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.ReceivedGroupBox = New System.Windows.Forms.GroupBox()
+        Me.ReceivedListBox = New System.Windows.Forms.ListBox()
+        Me.CommandGroupBox = New System.Windows.Forms.GroupBox()
+        Me.SendHexButton = New System.Windows.Forms.Button()
+        Me.HexCommandTextBox = New System.Windows.Forms.TextBox()
+        Me.StatusStrip.SuspendLayout()
+        Me.SerialSetupGroupBox.SuspendLayout()
+        Me.ReceivedGroupBox.SuspendLayout()
+        Me.CommandGroupBox.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'StatusStrip
+        '
+        Me.StatusStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SerialComStatusLabel})
+        Me.StatusStrip.Location = New System.Drawing.Point(0, 428)
+        Me.StatusStrip.Name = "StatusStrip"
+        Me.StatusStrip.Size = New System.Drawing.Size(800, 22)
+        Me.StatusStrip.TabIndex = 0
+        Me.StatusStrip.Text = "StatusStrip1"
+        '
+        'SerialComStatusLabel
+        '
+        Me.SerialComStatusLabel.Name = "SerialComStatusLabel"
+        Me.SerialComStatusLabel.Size = New System.Drawing.Size(108, 17)
+        Me.SerialComStatusLabel.Text = "Disconnected from"
+        '
+        'SerialSetupGroupBox
+        '
+        Me.SerialSetupGroupBox.Controls.Add(Me.ConnectDisconnectButton)
+        Me.SerialSetupGroupBox.Controls.Add(Me.SerialPortComboBox)
+        Me.SerialSetupGroupBox.Location = New System.Drawing.Point(12, 12)
+        Me.SerialSetupGroupBox.Name = "SerialSetupGroupBox"
+        Me.SerialSetupGroupBox.Size = New System.Drawing.Size(229, 56)
+        Me.SerialSetupGroupBox.TabIndex = 1
+        Me.SerialSetupGroupBox.TabStop = False
+        Me.SerialSetupGroupBox.Text = "COM Port"
+        '
+        'ConnectDisconnectButton
+        '
+        Me.ConnectDisconnectButton.Location = New System.Drawing.Point(136, 19)
+        Me.ConnectDisconnectButton.Name = "ConnectDisconnectButton"
+        Me.ConnectDisconnectButton.Size = New System.Drawing.Size(87, 21)
+        Me.ConnectDisconnectButton.TabIndex = 2
+        Me.ConnectDisconnectButton.Text = "Connect"
+        Me.ConnectDisconnectButton.UseVisualStyleBackColor = True
+        '
+        'SerialPortComboBox
+        '
+        Me.SerialPortComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.SerialPortComboBox.FormattingEnabled = True
+        Me.SerialPortComboBox.Location = New System.Drawing.Point(6, 19)
+        Me.SerialPortComboBox.Name = "SerialPortComboBox"
+        Me.SerialPortComboBox.Size = New System.Drawing.Size(124, 21)
+        Me.SerialPortComboBox.TabIndex = 2
+        '
+        'SerialPortRefreshTimer
+        '
+        Me.SerialPortRefreshTimer.Interval = 500
+        '
+        'ReceivedGroupBox
+        '
+        Me.ReceivedGroupBox.Controls.Add(Me.ReceivedListBox)
+        Me.ReceivedGroupBox.Location = New System.Drawing.Point(247, 12)
+        Me.ReceivedGroupBox.Name = "ReceivedGroupBox"
+        Me.ReceivedGroupBox.Size = New System.Drawing.Size(229, 162)
+        Me.ReceivedGroupBox.TabIndex = 2
+        Me.ReceivedGroupBox.TabStop = False
+        Me.ReceivedGroupBox.Text = "COM Port"
+        '
+        'ReceivedListBox
+        '
+        Me.ReceivedListBox.FormattingEnabled = True
+        Me.ReceivedListBox.Location = New System.Drawing.Point(6, 19)
+        Me.ReceivedListBox.Name = "ReceivedListBox"
+        Me.ReceivedListBox.Size = New System.Drawing.Size(217, 134)
+        Me.ReceivedListBox.TabIndex = 3
+        '
+        'CommandGroupBox
+        '
+        Me.CommandGroupBox.Controls.Add(Me.HexCommandTextBox)
+        Me.CommandGroupBox.Controls.Add(Me.SendHexButton)
+        Me.CommandGroupBox.Location = New System.Drawing.Point(12, 74)
+        Me.CommandGroupBox.Name = "CommandGroupBox"
+        Me.CommandGroupBox.Size = New System.Drawing.Size(229, 56)
+        Me.CommandGroupBox.TabIndex = 3
+        Me.CommandGroupBox.TabStop = False
+        Me.CommandGroupBox.Text = "Manual Send Hex"
+        '
+        'SendHexButton
+        '
+        Me.SendHexButton.Location = New System.Drawing.Point(136, 19)
+        Me.SendHexButton.Name = "SendHexButton"
+        Me.SendHexButton.Size = New System.Drawing.Size(87, 21)
+        Me.SendHexButton.TabIndex = 2
+        Me.SendHexButton.Text = "Send"
+        Me.SendHexButton.UseVisualStyleBackColor = True
+        '
+        'HexCommandTextBox
+        '
+        Me.HexCommandTextBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.HexCommandTextBox.Location = New System.Drawing.Point(6, 19)
+        Me.HexCommandTextBox.Name = "HexCommandTextBox"
+        Me.HexCommandTextBox.Size = New System.Drawing.Size(124, 20)
+        Me.HexCommandTextBox.TabIndex = 4
         '
         'VBSerialForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.CommandGroupBox)
+        Me.Controls.Add(Me.ReceivedGroupBox)
+        Me.Controls.Add(Me.SerialSetupGroupBox)
+        Me.Controls.Add(Me.StatusStrip)
         Me.Name = "VBSerialForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "PIC Serial Communications"
+        Me.StatusStrip.ResumeLayout(False)
+        Me.StatusStrip.PerformLayout()
+        Me.SerialSetupGroupBox.ResumeLayout(False)
+        Me.ReceivedGroupBox.ResumeLayout(False)
+        Me.CommandGroupBox.ResumeLayout(False)
+        Me.CommandGroupBox.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents StatusStrip As StatusStrip
+    Friend WithEvents SerialComStatusLabel As ToolStripStatusLabel
+    Friend WithEvents SerialSetupGroupBox As GroupBox
+    Friend WithEvents SerialPortComboBox As ComboBox
+    Friend WithEvents SerialPort As IO.Ports.SerialPort
+    Friend WithEvents SerialPortRefreshTimer As Timer
+    Friend WithEvents ConnectDisconnectButton As Button
+    Friend WithEvents ReceivedGroupBox As GroupBox
+    Friend WithEvents ReceivedListBox As ListBox
+    Friend WithEvents CommandGroupBox As GroupBox
+    Friend WithEvents SendHexButton As Button
+    Friend WithEvents HexCommandTextBox As TextBox
 End Class
--- Lab9-UART/VBSerial/VBSerial/VBSerial/VBSerialForm.vb
+++ Lab9-UART/VBSerial/VBSerial/VBSerial/VBSerialForm.vb
@@ -1,3 +1,114 @@
-﻿Public Class VBSerialForm
+﻿Option Explicit On
+Option Strict On
+Option Compare Text
 
+Public Class VBSerialForm
+    ''' <summary>
+    ''' Initialize the form. Starts the available serial port name refresh timer.
+    ''' Sets the SerialComStatusLabel to disconnected from current port.
+    ''' sets the serial port up for 9600 baud, 8bit data, one stop bit, and no parity bit
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub VBSerialForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SerialPortRefreshTimer.Start()
+        SerialComStatusLabel.Text = $"Disconnected from {SerialPort.PortName}"
+        SerialPort.BaudRate = 9600
+        SerialPort.DataBits = 8
+        SerialPort.StopBits = IO.Ports.StopBits.One
+        SerialPort.Parity = IO.Ports.Parity.None
+    End Sub
+
+    Private Sub SerialPortRefreshTimer_Tick(sender As Object, e As EventArgs) Handles SerialPortRefreshTimer.Tick
+        If Not SerialPort.IsOpen And Not SerialPortComboBox.DroppedDown Then
+            SerialPortComboBox.Items.Clear()
+            For Each sp As String In My.Computer.Ports.SerialPortNames
+                SerialPortComboBox.Items.Add(sp)
+            Next
+            SerialPortComboBox.Text = SerialPort.PortName
+        End If
+    End Sub
+
+    Private Sub ConnectDisconnectButton_Click(sender As Object, e As EventArgs) Handles ConnectDisconnectButton.Click
+        If SerialPort.IsOpen Then
+            SerialPort.Close()
+            ConnectDisconnectButton.Text = "Connect"
+            SerialComStatusLabel.Text = $"Disconnected from {SerialPort.PortName}"
+        Else
+            Try
+                SerialPort.Open()
+                ConnectDisconnectButton.Text = "Disconnect"
+                SerialComStatusLabel.Text = $"Connected to {SerialPort.PortName}"
+            Catch ex As Exception
+                MsgBox($"Failed to connect on {SerialPort.PortName}.{vbNewLine}{vbNewLine}Please select a valid COM port.")
+            End Try
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Occurs when the SerialPortComboBox value is changed. 
+    ''' 
+    ''' If the serial port is not open, the serial port will be set to the selected value.
+    ''' If the serial port is open and the new value is not the connected port, change the value back and prompt the user.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub SerialPortComboBox_SelectedValueChanged(sender As Object, e As EventArgs) Handles SerialPortComboBox.SelectedValueChanged
+        If SerialPort.IsOpen Then
+            If Not SerialPortComboBox.Text = SerialPort.PortName Then 'If the port is open and the serial port name does not match the connected
+                SerialPortComboBox.Text = SerialPort.PortName
+                MsgBox("Please disconnect before changing COM port.")
+            End If
+        Else 'if the port is closed
+            SerialPort.PortName = SerialPortComboBox.Text
+            SerialComStatusLabel.Text = $"Disconnected from {SerialPort.PortName}"
+        End If
+    End Sub
+
+    Private Sub HexCommandTextBox_TextChanged(sender As Object, e As EventArgs) Handles HexCommandTextBox.TextChanged
+        Dim hexInput As String
+        Dim formattedOutput As String = ""
+
+        hexInput = HexCommandTextBox.Text.Replace(" ", "")
+        If Not hexInput = "" Then
+            If Not "ABCDEF0123456789".Contains(hexInput.Last) Then
+                MsgBox($"{hexInput.Last} is not used in Hex. Please enter a Hex value.")
+                hexInput = hexInput.Substring(0, hexInput.Length - 1)
+            End If
+
+            'format the output to have spaces every two characters
+            For i = 0 To hexInput.Length - 1
+                formattedOutput &= hexInput(i)
+                'add a space after every two characters
+                If (i + 1) Mod 2 = 0 AndAlso i < hexInput.Length - 1 Then
+                    formattedOutput &= " "
+                End If
+            Next
+            HexCommandTextBox.Text = formattedOutput
+            HexCommandTextBox.Select(formattedOutput.Length, 0) 'move cursor back to end of input string
+        End If
+
+    End Sub
+
+    Private Sub SendHexButton_Click(sender As Object, e As EventArgs) Handles SendHexButton.Click
+        Dim hexArray() As String
+        hexArray = HexCommandTextBox.Text.Split(" "c)
+
+        Dim byteArray(hexArray.Length - 1) As Byte
+
+        Dim message As String = ""
+        If HexCommandTextBox.Text = "" Then
+            MsgBox("Please enter a Hex command.")
+        ElseIf HexCommandTextBox.Text.Replace(" ", "").Length Mod 2 <> 0 Then
+            MsgBox("There must be an even number of characters.")
+        Else
+            For i = 0 To hexArray.Length - 1
+                byteArray(i) = CByte(Convert.ToInt32(hexArray(i), 16))
+                message &= byteArray(i) & vbNewLine
+            Next
+            MsgBox(message)
+            'MsgBox(Convert.ToInt32(HexCommandTextBox.Text, 16))
+            'MsgBox(CByte(Convert.ToInt32(HexCommandTextBox.Text, 16)))
+        End If
+    End Sub
 End Class
